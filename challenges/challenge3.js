edc4fffe8b7572b4dd0f6b2c959a5c485e75874e
shahrour <ahmadshahrour951@gmail.com>
2021-01-30 10:29:14 -0800

    Update challenge3.js

--- challenges/challenge3.js
+++ challenges/challenge3.js
@@ -6,13 +6,16 @@
  * 1. Read over the `greetAndUppercase` function. This function uses
  *    Async/Await. How is this function different than a regular (non-async)
  *    function? What is its return type?
- * 
+ *    The async function is different in that it can handle asynchronous code using the await keyword, just like how a then block works on in promise chaining.
+ *    return type is a promise
  * 
  * 2. Uncomment block #1 and run the code using `node challenge3.js`. What is
  *    printed when we use `greetAndUppercase` like a regular function?
+ *    a pending promise is printed, that is because console.log does not wait for the promise to resolve.
  * 
  * 
  * 3. Uncomment block #2 and run the code again. What happens now?
+ *    The async function is resovled and it is chained with a .then() because it is a promise, in there the resolved promise is console logged.
  * 
  * 
  * 4. Write an asynchronous method 'spacer' that takes a string as input and 
@@ -61,21 +64,25 @@ function uppercaser(str) {
     });
 }
 
+async function spacer(str) {
+  return str.split('').join(' ');
+}
+
 async function greetAndUppercase(name) {
     greeting = await greet(name)
     uppercasedGreeting = await uppercaser(greeting)
-    return uppercasedGreeting
+    return spacer(uppercasedGreeting)
 }
 
 /* Uncomment me! #1 */
-// result = greetAndUppercase('Ducky')
-// console.log(result)
+result = greetAndUppercase('Ducky')
+console.log(result)
 
 /* Uncomment me! #2 */
-// greetAndUppercase('Ducky')
-//     .then(function(result) {
-//         console.log(result)
-//     })
-//     .catch(function(err) {
-//         console.log(err)
-//     })
+greetAndUppercase('Ducky')
+    .then(function(result) {
+        console.log(result)
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
